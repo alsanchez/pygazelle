93502425a85d34f2805ff6d7f25e1ec3e8137f03
Alejandro Sánchez <contacto@alsanchez.es>
2013-04-28 12:33:59 +0200

    Add distutils setup script.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="pygazelle",
+      version="1.0",
+      description="A python library exposing the functionality of the what.cd Gazelle JSON API",
+      author="cohena",
+      url="https://github.com/cohena/pygazelle",
+      packages=["pygazelle"])
